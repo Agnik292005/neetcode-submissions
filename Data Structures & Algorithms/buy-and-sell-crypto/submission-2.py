f43d2d4c71340e0dd5157daf8d4c5271fd66f820
Agnik Patra <agnik292005@gmail.com>
2026-06-05 04:48:59 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        m=prices[0]
+        mx=0
+        ans=[0]*len(prices)
+        for i in range(len(prices)):
+            if prices[i]<m:
+                m=prices[i]
+            ans[i]=m
+            if prices[i]-ans[i]>mx:
+                mx=prices[i]-ans[i]
+            
+        return mx
+
+        
