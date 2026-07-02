be143c7fc71aa59a5be5540ff25ce59b6a66848f
Agnik Patra <agnik292005@gmail.com>
2026-07-02 05:57:19 +0530

    Add: lemonade-change - submission-4

---
+++ Data Structures & Algorithms/lemonade-change/submission-4.py
@@ -0,0 +1,27 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        fives=0
+        tens=0
+        twenties=0
+        for i in bills:
+            if i==5:
+                fives+=1
+            elif i==10:
+                tens+=1
+                if fives<=0:
+                    return False
+                else:
+                    fives-=1
+            elif i==20:
+                twenties+=1
+                if tens<=0 and fives<=0:
+                    return False
+                elif fives>=3:
+                    fives-=3
+                elif fives>0 and tens>0:
+                    tens-=1
+                    fives-=1
+                else:
+                    return False
+                
+        return True
